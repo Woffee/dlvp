b06413b21ccd22f82ef89d59b8290c1ae5481e9f
wenbo <iwoffee@gmail.com>
2021-07-09 23:44:57 -0400

    remove useless code

--- gcn.py
+++ gcn.py
@@ -65,12 +65,6 @@ class GNNStack(nn.Module):
         self.lp_length = lp_length
         self.ns_length = ns_length
 
-        # LP: (lp_path_num, lp_length, lp_dim)
-        # self.lp_conv1 = nn.Conv2d(lp_path_num, 6, kernel_size=5, padding=2) # (6, lp_length, lp_dim)
-        # self.lp_pool1 = nn.MaxPool2d(2, 2)
-        # self.lp_conv2 = nn.Conv2d(6, 16, kernel_size=5, padding=2) # (16, lp_length / 2, lp_dim / 2)
-        # self.lp_pool2 = nn.MaxPool2d(2, 2)
-        # self.lp_fc = nn.Linear(16 * (lp_length/4) * (lp_dim/4), 128)
         self.lp_grus = nn.ModuleList()
         for l in range(lp_path_num):
             """
@@ -81,12 +75,6 @@ class GNNStack(nn.Module):
             self.lp_grus.append(nn.GRU(input_size=lp_dim, hidden_size=lp_dim, num_layers=1, batch_first=True))
         self.lp_fc = nn.Linear(lp_dim * lp_path_num, 128)
 
-        # NS: (1, ns_length, ns_dim)
-        # self.ns_conv1 = nn.Conv2d(1, 6, kernel_size=5, padding=2)  # (6, ns_length, ns_dim)
-        # self.ns_pool1 = nn.MaxPool2d(2, 2)
-        # self.ns_conv2 = nn.Conv2d(6, 16, kernel_size=5, padding=2)  # (16, ns_length / 2, ns_dim / 2)
-        # self.ns_pool2 = nn.MaxPool2d(2, 2)
-        # self.ns_fc = nn.Linear(16 * (ns_length / 4) * (ns_dim / 4), 128)
         self.ns_gru = nn.GRU(lp_dim, lp_dim, 1, batch_first=True)
 
         # 5 个 embedding 合到一起
@@ -132,8 +120,8 @@ class GNNStack(nn.Module):
 
         # DEF, REF, PDT, LP, NS --> x
         x_lp, x_ns, x_ref, x_def, x_pdt = data.x_lp, data.x_ns, data.x_ref, data.x_def, data.x_pdt
-        print("x_ns:", x_ns.shape)
-        print("x_lp:", x_lp.shape)
+        # print("x_ns:", x_ns.shape)
+        # print("x_lp:", x_lp.shape)
 
         # 当前 batch 总共有多少个 function。因为每个 graph 含有不同数量的 function，因此这里数值不定。
         cur_nodes_size = x_ns.shape[0]
@@ -153,19 +141,19 @@ class GNNStack(nn.Module):
         # gru_output_list = gru_output_list.view(64, 128 * 20)
         x_lp = torch.cat(gru_output_list).view(x_ns.shape[0], 128 * 20)
         x_lp = self.lp_fc(x_lp)
-        print("x_lp_after:", x_lp.shape)
+        # print("x_lp_after:", x_lp.shape)
 
         # NS
         out, h_ns = self.ns_gru(x_ns)
-        print("h_ns:", h_ns.shape)
+        # print("h_ns:", h_ns.shape)
         x_ns = h_ns.view(cur_nodes_size, 128)
 
         # concatenate together
         x = torch.cat([x_pdt, x_ref, x_def, x_lp, x_ns]).view(cur_nodes_size, 128 * 5)
         x = self.all_fc(x)
 
-        print("x_concatenated:", x.shape)
-        print("edge_index:", edge_index.shape)
+        # print("x_concatenated:", x.shape)
+        # print("edge_index:", edge_index.shape)
 
         for i in range(self.num_layers):
             x = self.convs[i](x, edge_index)
@@ -175,14 +163,14 @@ class GNNStack(nn.Module):
             x = F.dropout(x, p=self.dropout, training=self.training)
             if not i == self.num_layers - 1:
                 x = self.lns[i](x)
-        print("x after convs:", x.shape)
+        # print("x after convs:", x.shape)
         if self.task == 'graph':
             x = pyg_nn.global_mean_pool(x, batch)
-        print("batch:", batch)
-        print("x after global_mean_pool:", x.shape)
+        # print("batch:", batch)
+        # print("x after global_mean_pool:", x.shape)
 
         x = self.post_mp(x)
-        print("x_final.shape:", x.shape)
+        # print("x_final.shape:", x.shape)
         return emb, F.log_softmax(x, dim=1)  # dim (int): A dimension along which log_softmax will be computed.
 
     def loss(self, pred, label):
@@ -295,8 +283,8 @@ def train(dataset, task, writer):
             if task == 'node':
                 pred = pred[batch.train_mask]
                 label = label[batch.train_mask]
-            print("pred.shape : ",pred.shape)
-            print("label.shape: ",label.shape)
+            # print("pred.shape : ",pred.shape)
+            # print("label.shape: ",label.shape)
 
             loss = model.loss(pred, label)
             loss.backward()
@@ -314,148 +302,6 @@ def train(dataset, task, writer):
             writer.add_scalar("test accuracy", test_acc, epoch)
 
     return model
-
-
-def read_my_data(save_path):
-    """
-    :param save_path: '/xye_data_nobackup/wenbo/dlvp/data/function2vec'
-    :return:
-    """
-
-    all_funcs_lp_file = save_path + "/all_functions_with_trees_lp.csv"
-
-    emb_file_def = save_path + "/all_func_embedding_file_def.csv"
-    emb_file_ref = save_path + "/all_func_embedding_file_ref.csv"
-    emb_file_pdt = save_path + "/all_func_embedding_file_pdt.csv"
-
-    emb_file_lp_combine = save_path + "/all_func_embedding_file_lp.pkl.combine"
-    emb_file_lp_greedy = save_path + "/all_func_embedding_file_lp.pkl.greedy"
-
-    emb_file_ns = save_path + "/all_func_embedding_file_ns.pkl"
-
-
-    # read files
-    df_all_funcs = pd.read_csv(all_funcs_lp_file)
-
-    df_emb_def = pd.read_csv(emb_file_def).drop(columns=['type']).values
-    df_emb_ref = pd.read_csv(emb_file_ref).drop(columns=['type']).values
-    df_emb_pdt = pd.read_csv(emb_file_pdt).drop(columns=['type']).values
-
-    emb_lp_combine = pickle.load(open(emb_file_lp_combine,"rb"))
-    emb_lp_greedy  = pickle.load(open(emb_file_lp_greedy,"rb"))
-
-    emb_ns = pickle.load(open(emb_file_ns, "rb"))
-
-    print( "len(df_all_funcs):", len(df_all_funcs) )
-
-    print( "len(df_emb_def):", len(df_emb_def) )
-    print( "len(df_emb_ref):", len(df_emb_ref) )
-    print( "len(df_emb_pdt):", len(df_emb_pdt) )
-
-    print( "len(emb_lp_combine):", len(emb_lp_combine) )
-    print( "len(emb_lp_greedy):", len(emb_lp_greedy) )
-
-    print( "len(emb_ns):", len(emb_ns) )
-
-
-    """
-    len(df_all_funcs): 15719
-    len(df_emb_def): 15719
-    len(df_emb_ref): 15719
-    len(df_emb_pdt): 15719
-    len(emb_lp_combine): 14608
-    len(emb_lp_greedy): 14608
-    len(emb_ns): 15719
-    """
-
-    x = []
-    edge_index = []
-    y = []
-    xs = []
-
-    x_lp = []
-    x_ns = []
-    x_def = []
-    x_ref = []
-    x_pdt = []
-
-    dataset = []
-    for index, row in df_all_funcs.iterrows():
-        func_id = int(row['func_id'])
-        if func_id > 1000:
-            break
-        if not (func_id in emb_lp_combine.keys() and func_id in emb_ns.keys()):
-            continue
-
-        x.append([0])
-        if row['callees'].strip() != "[]":
-            # logger.info("== callees != []")
-            callees = row['callees'].strip().replace("[", "").replace("]","").replace(" ", "" ).split(",")
-            # logger.info("== len(callees):%d" % len(callees))
-            for ce in callees:
-                edge_index.append([ int(row['func_id']), int(ce) ])
-        if row['callers'].strip() != "[]":
-            callers = row['callers'].strip().replace("[", "").replace("]","").replace(" ", "" ).split(",")
-            for cr in callers:
-                edge_index.append([ int(cr), int(row['func_id']) ])
-
-        # xs.append({
-        #     'lp': emb_lp_combine[func_id],
-        #     'ns': emb_ns[func_id],
-        #     'def': df_emb_def[index],
-        #     'ref': df_emb_ref[index],
-        #     'pdt': df_emb_pdt[index]
-        # })
-        # x_lp_emb = torch.zeros([LP_PATH_NUM, 60, 128], dtype=torch.float)
-        x_lp_emb = np.zeros( (LP_PATH_NUM, 60, 128), dtype=float)
-
-        for i in range(len(emb_lp_combine[func_id])):
-            if i >= LP_PATH_NUM:
-                break
-            x_lp_emb[i] = torch.tensor(emb_lp_combine[func_id][i], dtype=torch.float)
-
-        x_lp.append(x_lp_emb)
-        x_ns.append(emb_ns[func_id])
-        x_def.append(df_emb_def[index])
-        x_ref.append(df_emb_ref[index])
-        x_pdt.append(df_emb_pdt[index])
-
-        if int(row['is_center']) == 1:
-            # y.append(int(row['vul']))
-            # edge_index = np.array(edge_index)
-            # edge_index = edge_index - edge_index.min()
-            x = torch.tensor(x, dtype=torch.float)
-            x_lp = torch.tensor(np.array(x_lp), dtype=torch.float)
-            x_ns = torch.tensor(x_ns, dtype=torch.float)
-            x_def = torch.tensor(x_def, dtype=torch.float)
-            x_ref = torch.tensor(x_ref, dtype=torch.float)
-            x_pdt = torch.tensor(x_pdt, dtype=torch.float)
-
-            edge_index = torch.tensor(edge_index, dtype=torch.long)
-            if edge_index.shape[0] > 0:
-                edge_index = edge_index - edge_index.min()
-
-            if int(row['vul']) == 1:
-                y = torch.tensor([1, 0], dtype=torch.long)
-            else:
-                y = torch.tensor([0, 1], dtype=torch.long)
-
-            data = Data(x=x, edge_index=edge_index.t().contiguous(), y=y, x_pdt = x_pdt, x_ref=x_ref, x_def=x_def, x_lp = x_lp, x_ns = x_ns)
-            dataset.append(data)
-            x = []
-            edge_index = []
-            y = []
-            xs = []
-            x_lp = []
-            x_ns = []
-            x_def = []
-            x_ref = []
-            x_pdt = []
-
-    # loader = DataLoader(dataset, batch_size=2, shuffle=True)
-    # return loader
-
-    return dataset
 
 def findAllFile(base):
     for root, ds, fs in os.walk(base):
@@ -663,45 +509,11 @@ class MyLargeDataset(Dataset):
         return data
 
 if __name__ == '__main__':
-    # save_path = '/xye_data_nobackup/wenbo/dlvp/data/function2vec'
-    # save_path = BASE_DIR + "/data/function2vec"
-    # read_my_data(save_path)
-    # exit()
-
-
-    # edge_index = torch.tensor([[10, 11],
-    #                            [11, 10],
-    #                            [11, 12],
-    #                            [12, 11]], dtype=torch.long)
-    # x = torch.tensor([[-1], [0], [1]], dtype=torch.float)
-    # xs = [
-    #     {
-    #         'lp': torch.tensor([[-1], [0], [1]], dtype=torch.float)
-    #     },
-    #     {
-    #         'lp': torch.tensor([[-1], [0], [1]], dtype=torch.float)
-    #     },
-    #     {
-    #         'lp': torch.tensor([[-1], [0], [1]], dtype=torch.float)
-    #     },
-    # ]
-    # data = Data(x=x, edge_index=edge_index.t().contiguous(), xs = xs)
-    # loader = DataLoader([data], batch_size=2, shuffle=True)
-    # for batch_data in loader:
-    #     xs, edge_index, batch = batch_data.xs, batch_data.edge_index, batch_data.batch
-    #     print("???", xs)
-    #     print("===", edge_index)
-    # exit()
-
-
     writer = SummaryWriter("./log/" + datetime.now().strftime("%Y%m%d-%H%M%S"))
-
-    # dataset = TUDataset(root='/tmp/PROTEINS', name='PROTEINS')
-    # dataset = dataset.shuffle()
 
     task = 'graph'
     save_path = BASE_DIR + "/data/function2vec"
-    # dataset = read_my_data(save_path)
+
     dataset = MyLargeDataset(save_path)
     model = train(dataset, task, writer)
     logger.info("training GCN done.")
