2b6cba77b9cbc21470d7564e0105cdcd59d15e0e
wenbo <iwoffee@gmail.com>
2021-07-21 18:36:27 -0400

    add sync folder

--- run.sh
+++ run.sh
@@ -1,45 +1,28 @@
 #now_time=`date +"%Y-%m-%d-%H-%M"`
-#
+
+# Cflow
 #data_type="linux"
 #log_file="../log/${data_type}_${now_time}.log"
-#
 #echo python cflow.py --log_file ${log_file} --data_type ${data_type}
 #python cflow.py --log_file ${log_file} --data_type ${data_type}
 
 
 # Functions to vec
+#python function2vec2.py --cur_p 9 \
+#--tasks_file cflow/tasks.json \
+#--all_func_trees_file data/function2vec2/all_functions_with_trees.csv
 
-#python function2vec.py --cve_jsons_path data/jsons/FFmpeg_jsons \
-#--embedding_type ref \
-#--all_functions_file data/function2vec/all_functions.csv \
-#--all_func_trees_file data/function2vec/all_functions_with_trees.csv \
-#--all_func_trees_json_file data/function2vec/all_functions_with_trees.json \
-#--all_func_embedding_file data/function2vec/all_func_embedding_file_ref.csv
-
-#python function2vec.py --cve_jsons_path data/jsons/FFmpeg_jsons \
-#--embedding_type def \
-#--all_functions_file data/function2vec/all_functions.csv \
-#--all_func_trees_file data/function2vec/all_functions_with_trees.csv \
-#--all_func_trees_json_file data/function2vec/all_functions_with_trees.json \
-#--all_func_embedding_file data/function2vec/all_func_embedding_file_def.csv
-
-#python function2vec.py --cve_jsons_path data/jsons/FFmpeg_jsons \
-#--embedding_type pdt \
-#--all_functions_file data/function2vec/all_functions.csv \
-#--all_func_trees_file data/function2vec/all_functions_with_trees.csv \
-#--all_func_trees_json_file data/function2vec/all_functions_with_trees.json \
-#--all_func_embedding_file data/function2vec/all_func_embedding_file_pdt.csv
-
-#python function2vec.py --cve_jsons_path data/jsons/FFmpeg_jsons \
-#--embedding_type lp \
-#--all_functions_file data/function2vec/all_functions.csv \
-#--all_func_trees_file data/function2vec/all_functions_with_trees.csv \
-#--all_func_trees_json_file data/function2vec/all_functions_with_trees.json \
-#--all_func_embedding_file data/function2vec/all_func_embedding_file_lp.pkl
-
-python function2vec.py --cve_jsons_path data/jsons/FFmpeg_jsons \
---embedding_type ns \
---all_functions_file data/function2vec/all_functions.csv \
---all_func_trees_file data/function2vec/all_functions_with_trees.csv \
---all_func_trees_json_file data/function2vec/all_functions_with_trees.json \
---all_func_embedding_file data/function2vec/all_func_embedding_file_ns.pkl
+# Run GCN
+python gcn.py --tasks_file /data/function2vec2/tasks.json \
+  --functions_path /data/function2vec2/functions_jy \
+  --model_save_path /data/gcn_models \
+  --input_dim 128 \
+  --hidden_dim 128 \
+  --batch_size 16 \
+  --lp_path_num 20 \
+  --lp_length 60 \
+  --lp_dim 128 \
+  --lp_w2v_path /data/function2vec2/models/w2v_lp_combine.bin \
+  --ns_length 2000 \
+  --ns_dim 128 \
+  --ns_w2v_path /data/function2vec2/models/w2v_ns_combine.bin
